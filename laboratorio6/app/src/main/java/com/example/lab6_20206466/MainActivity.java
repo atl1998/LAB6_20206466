1f3cbe65ef610625cb017d0a3bbefd07dbd05b37
atl1998 <adrianbala0410@gmail.com>
2025-06-17 10:35:01 -0500

    fragments
    botton nav, ffragments e iconos

--- laboratorio6/app/src/main/java/com/example/lab6_20206466/MainActivity.java
+++ laboratorio6/app/src/main/java/com/example/lab6_20206466/MainActivity.java
@@ -2,23 +2,56 @@ package com.example.lab6_20206466;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        // Cargar fragmento por defecto
+        loadFragment(new Linea1Fragment());
+
+        // Listener con if-else
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.linea1) {
+                selectedFragment = new Linea1Fragment();
+            } else if (itemId == R.id.limaPass) {
+                selectedFragment = new LimaPassFragment();
+            } else if (itemId == R.id.resumen) {
+                selectedFragment = new ResumenFragment();
+            } else if (itemId == R.id.logout) {
+                selectedFragment = new LogoutFragment();
+            }
+
+            if (selectedFragment != null) {
+                loadFragment(selectedFragment);
+                return true;
+            } else {
+                return false;
+            }
         });
     }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fragmentContainer, fragment);
+        ft.commit();
+    }
 }
+
